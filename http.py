db3dac3cd04f4a2003b8f4407c46c4e5621da4a5
chaemin2003 <c20580@kma.ac.kr>
2025-03-29 19:58:57 +0900

    리디렉션, 오류 처리
    1. 301,302 처리
    2. https 제한
    3. 리디렉션 5회 제한
    4. 리디렉션 구현

--- http.py
+++ http.py
@@ -38,3 +38,21 @@ decoded = response.decode(errors = "ignore")
 headr_data, _, body = decoded.partition("\r\n\r\n")
 status_line = header_data.split("\r\n")[0]
 status_code = int(status_line.split()[1])
+
+
+if status_code in (301, 302):
+  location = ""
+  for line in header_data.split("\r\n"):
+    if line.lower(). startswith("location:"):
+      location = line.split(":",1)[1].strip()
+  print(f"Redirected to : {location}", file=sys.stderr)
+
+  if location.startswith("https://"):
+    print("HTTPS 주소 불가", file=sys.stderr)
+    sys.exit(1)
+
+  if redirect_count>=5:
+    print("redirect 5회 이상", file=sys.stderr)
+    sys.exit(1)
+
+  return fetch_http(location, redirect_count+1)
