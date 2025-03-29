87c433bcc51b49262e50dd557594e900c3cc2d73
chaemin2003 <c20580@kma.ac.kr>
2025-03-29 12:30:41 +0900

    url 불러와 조건 확인

--- http.py
+++ http.py
@@ -4,3 +4,9 @@ import socket
 if len(sys.argv) != 2:
   print("python http.py http://주소 형식 입력", file=sys.stderr)
   sys.exit(1)
+
+url = sys.argv[1]
+
+if not url.startswith("http://"):
+  print("http:// 로 시작", file = sys.stderr)
+  sys.exit(1)
