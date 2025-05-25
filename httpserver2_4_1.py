e9228e5ef08d416e3a3b2abb550461da9e362897
chaemin2003 <c20580@kma.ac.kr>
2025-05-25 22:03:07 +0900

    서버 클래스 정의

--- httpserver2_4_1.py
+++ httpserver2_4_1.py
@@ -2,3 +2,15 @@ import socket
 import select
 import sys
 import os
+
+class MultiConnectionHTTPServer:
+  def __init__(self, port):
+    self.port = port
+    self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.server_socket.setsockopt(socket.SOL_SOCKNET, socket.SO_REUSEADDR, 1)
+    self.server_socket.bind(('', self.port))
+    self.server_socket.listen(5)
+    self.server_socket.setblocking(False)
+    self.open_connections = {}
+
+
