a8f6360b687c4214abed3315d2de769705b7c0ae
chaemin2003 <c20580@kma.ac.kr>
2025-05-25 22:18:57 +0900

    서버 루프, 읽기 소켓 처리

--- httpserver2_4_1.py
+++ httpserver2_4_1.py
@@ -13,4 +13,18 @@ class MultiConnectionHTTPServer:
     self.server_socket.setblocking(False)
     self.open_connections = {}
 
+  def serve_forever(self):
+    inpus = [self.server_socket]
+    print(f"[서버 시작] 포트 {self.port}에서 대기 중...")
+    while True:
+      readables, _, _ = select.select(inputs, [], [])
+
+      for sock in readables:
+        if sock is self.server_socket:
+          client_socket, client_address = self.server_socket.accept()
+          print(f"[접속 수락] {client_address} 연결됨")
+          client_socket.setblocking(False)
+          inputs.append(client_socket)
+          self.open_connections[client_socket] = client_address
+
 
