579624af86afddd9236f26e080f490d1c64e769d
chaemin2003 <c20580@kma.ac.kr>
2025-05-25 23:06:12 +0900

    들여쓰기 오류 수정, 오타 수정

--- httpserver2_4_1.py
+++ httpserver2_4_1.py
@@ -37,28 +37,34 @@ class MultiConnectionHTTPServer:
 
             request_line = request.split("\r\n")[0]
             parts = request_line.split()
+            
             if len(parts) != 3:
               self.send_error(sock, 400, "Bad Request")
               continue
+              
             method, path, version = parts
+            
             if method != "GET":
               self.send_error(sock, 405, "Method Not Allowed")
               continue
 
             filename = path.lstrip('/')
-            if not (filename.endswith('.html') or filename.endswith('.htm)):
+            
+            if not (filename.endswith('.html') or filename.endswith('.htm')):
               self.send_error(sock, 403, "Forbidden")
               continue
+              
             if not os.path.exists(filename):
               self.send_error(sock, 404, "Not Found")
               continue
+              
             with open(filename, 'rb') as f:
               body = f.read()
 
             header = "HTTP/1.0 200 OK\r\n"
             header += "Content-Type: text/html\r\n"
             heaer += f"content-Length: {len(body)}\r\n"
-            header += "r\n\"
+            header += "\r\n"
 
             sock.sendall(header.encode() +body)
             print(f"[응답 완료] {filename}")
@@ -72,36 +78,42 @@ class MultiConnectionHTTPServer:
             inputs.remove(sock)
             del self.open_connections[sock]
           
-                                                                                                                                 
-    def send_error(self, sock, code, message):
-      body = f"<html><body><h1>{code} {message}</h1></body></html>".encode()
-      header = f"HTTP/1.0 {code} {message}\r\n"
-      header += "Content-Type: text/html\r\n"
-      header += f"Content-Length: {len(body)}\r\n"
-      header += "\r\n"
-      response = header.encode() + body
-      try:
-        sock.sendall(response)
-      except:
-        pass
+
+
+    
+  def send_error(self, sock, code, message):
+    body = f"<html><body><h1>{code} {message}</h1></body></html>".encode()
+    header = f"HTTP/1.0 {code} {message}\r\n"
+    header += "Content-Type: text/html\r\n"
+    header += f"Content-Length: {len(body)}\r\n"
+    header += "\r\n"
+    response = header.encode() + body
+      
+    try:
+      sock.sendall(response)
+        
+    except:
+      pass
+
+
 
 if __name__ == "__main__":
-        if len(sys.argv) != 2:
-          print("사용법 : python httpserver2_4_1.py [포트번호]")
-          sys.exit(1)
+  if len(sys.argv) != 2:
+    print("사용법 : python httpserver2_4_1.py [포트번호]")
+    sys.exit(1)
 
-        try:
-          port = int(sys.argv[1])
-          if port < 1024:
-            print("포트 번호는 1024 이상이어야 합니다.")
-            sys.exit(1)
+  try:
+    port = int(sys.argv[1])
+    if port < 1024:
+      print("포트 번호는 1024 이상이어야 합니다.")
+      sys.exit(1)
 
-        except:
-          print("포트 번호는 숫자여야 합니다.")
-          sys.exit(1)
+  except:
+    print("포트 번호는 숫자여야 합니다.")
+    sys.exit(1)
 
-        server = MultiConnectionHTTPServer(port)
-        server.serve_forever()
+  server = MultiConnectionHTTPServer(port)
+  server.serve_forever()
 
 
             
