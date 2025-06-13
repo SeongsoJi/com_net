ccf2ff81e87fa55140619ebd6df3a814109502ca
chaemin2003 <c20580@kma.ac.kr>
2025-06-14 00:22:32 +0900

    프록시 메인

--- dnsproxy_4_2.py
+++ dnsproxy_4_2.py
@@ -9,3 +9,28 @@ class DNSProxy:
     self.fake_ip = fake_ip
     self.buffer_size = 512
     self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+  def start(self):
+    try:
+      self.server_socket.bind((self.listen_host, self.listen_port))
+      print("DNS 프록시가 {self.listen_host}:{self.listen_port}에서 수신 대기 중입니다")
+
+      while True:
+        data, client_addr = self.server_socket.recvform(self.buffer_size)
+        print(f"{client_addr}로부터 DNS 요청을 받았습니다")
+        
+        self.server_socket.sendto(data, self.upstream_dns)
+        response, _ = self.server_socket.recvfrom(self.buffer_size)
+        
+        if self.is_nxdomain(response):
+          print("NXDOMAIN(도메인 없음) 응답 감지됨 → 조작된 응답을 전송합니다")
+          response = self.craft_fake_response(data)
+
+        self.server_socket.sendto(response, client_addr)
+        print(f"{client_addr}에게 응답을 전송했습니다")
+
+    except KeyboardInterrupt:
+      print("DNS 프록시를 종료합니다.")
+    finally:
+      self.server_socket.close()
