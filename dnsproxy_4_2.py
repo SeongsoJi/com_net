e3b65f8b8e0f5589398818d2bbb9bad3ee8746f1
chaemin2003 <c20580@kma.ac.kr>
2025-06-16 01:29:25 +0900

    1.1.1.1

--- dnsproxy_4_2.py
+++ dnsproxy_4_2.py
@@ -2,7 +2,7 @@ import socket
 import sys
 
 class DNSProxy:
-    def __init__(self, listen_host='0.0.0.0', listen_port=1053, upstream_dns=('8.8.8.8', 53), fake_ip='203.0.113.1'):
+    def __init__(self, listen_host='0.0.0.0', listen_port=1053, upstream_dns=('1.1.1.1', 53), fake_ip='203.0.113.1'):
         self.listen_host = listen_host
         self.listen_port = listen_port
         self.upstream_dns = upstream_dns
